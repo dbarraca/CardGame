70217aa6d1a3b413b63e19a37b5d21699f960fd4
dbarraca <daniel.barraca@gmail.com>
2021-08-02 19:14:48 -0700

    Changed user fetch user axios

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,18 +1,26 @@
+import axios from 'axios';
 import { FETCH_USERS, UPDATE_USER } from './types.js';
 
 export const fetchUsers = () => async dispatch => {
     // const API = process.env.NODE_ENV === 'production' ? 'https://heart-of-the-cards.herokuapp.com' : 'http://localhost:5000';
 
     // const res = await fetch(`${API}/users/ai`, {
-    const res = await fetch('/users/ai', {
+    /*const res = await fetch('/users/ai', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         } 
-    });
+    });*/
 
-    const users = await res.json();
+    // const config = {
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    // }
 
+    const { data }  = await axios.get('/users/ai');
+
+    const users = await data;
 
     dispatch({
         type: FETCH_USERS,
