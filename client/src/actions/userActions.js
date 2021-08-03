27a517a3820f73912e365b3b64763e921f2da266
dbarraca <daniel.barraca@gmail.com>
2021-08-02 18:50:24 -0700

    Update user fetch in client

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,9 +1,10 @@
 import { FETCH_USERS, UPDATE_USER } from './types.js';
 
 export const fetchUsers = () => async dispatch => {
-    const API = process.env.NODE_ENV === 'production' ? 'https://heart-of-the-cards.herokuapp.com' : 'http://localhost:5000';
+    // const API = process.env.NODE_ENV === 'production' ? 'https://heart-of-the-cards.herokuapp.com' : 'http://localhost:5000';
 
-    const res = await fetch(`${API}/users/ai`, {
+    // const res = await fetch(`${API}/users/ai`, {
+    const res = await fetch('/users/ai', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
