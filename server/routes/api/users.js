4f0a31a78b9904292f4918588b5bd9d951e578d8
dbarraca <daniel.barraca@gmail.com>
2021-08-12 16:13:43 -0700

    Edited jwt secret retrieval

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -41,17 +41,23 @@ router.post('/', async (req, res) => {
         newUser.save();
         
         if (process.env.NODE_ENV === 'production') {
-            let JWTSecret = process.env.jwtSecret;
+            const token = await jwt.sign(
+                { id: newUser.id },
+                process.env.jwtSecret,
+                { expiresIn: 3600 }
+            )
+            .then(() => console.log('User Authencicated'))
+            .catch(err => console.log(err));
         }
         else {
-            let JWTSecret = config.get('jwtSecret');
+            const token = await jwt.sign(
+                { id: newUser.id },
+                config.get('jwtSecret'),
+                { expiresIn: 3600 }
+            )
+            .then(() => console.log('User Authencicated'))
+            .catch(err => console.log(err));
         }
-
-        const token = await jwt.sign(
-            { id: newUser.id },
-            JWTSecret,
-            { expiresIn: 3600 }
-        );
 
         return res.status(200).json({newUser: {
             token,
