ae0496639d0a4691f4df1aa5698fe2e405c3793b
dbarraca <daniel.barraca@gmail.com>
2021-08-12 22:55:37 -0700

    Set to load user only when user not loaded

--- client/src/components/Routing.js
+++ client/src/components/Routing.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux'; 
+import { useDispatch, useSelector } from 'react-redux'; 
 import Table from './Table';
 import Login from './Login';
 import SignUp from './SignUp';
@@ -9,9 +9,11 @@ import { loadUser } from '../actions/authActions';
 
 const Routing = () => {
     const dispatch = useDispatch();
+    const token = useSelector(state => state.token);
 
     useEffect(() => {
-      dispatch(loadUser());
+        if (!token)
+            dispatch(loadUser());
   
     }, [dispatch, loadUser] );
   
