7e5f8578f2d03ef848481fdf2afc807260aece42
dbarraca <daniel.barraca@gmail.com>
2021-08-18 22:35:26 -0700

    Moved load user dispatch

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.scss';
 import Routing from './components/Routing';
-
 import { Provider, useDispatch } from 'react-redux'; 
 import store from './store';
 
 function App() {
+  // useEffect(() => {
+  //   store.dispatch(loadUser());
+  // }, []);
+
   return (
     <Provider store={store}>
         <div className="App">
